1fd9063395ea5ccbe51bdc8ffca4a77e7a234826
virender_maurya <vrndrmry@gmail.com>
2023-07-15 23:46:22 +0530

    first commit

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/login", async (req, res) => {
       if (err) {
         return res.status(400).json(err);
       } else {
-        return res.cookie("token", token)
+        return res.cookie("token", token, {sameSite:"None"})
         .json({
           id: userDoc._id,
           name: userDoc.name,
